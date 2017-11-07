2f2e71e214f2b3feb393cc4d1c9864d8e406686d
Idan Yalovich <idan@ystudiodesign.com>
2017-11-07 20:54:42 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1>
-      Welcome to {{title}}!!
-    </h1>
-  `,
-  styles: []
+  selector: 'hi-root',
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+
+
+
+
+
 }
---
+++ src/app/news/news.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news',
+  template: `
+    <p>
+      news Works!
+    </p>
+  `,
+  styles: []
+})
+export class NewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
